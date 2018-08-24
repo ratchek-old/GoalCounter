f311256fb4b7c8c34bedddec0087de972477974f
ratchek <git@ratchek.com>
2018-08-24 01:04:26 -0400

    reorganize code. add new entries to json

--- script.js
+++ script.js
@@ -5,29 +5,54 @@ var rewards_text = '{ "rewards" : ['+
 
 var rewards = JSON.parse(rewards_text);
 
-var inc_button = document.getElementById("clickme"),
-  count = 0;
-inc_button.onclick = function() {
+function inc() {
   count += 1;
   inc_button.innerHTML = "Click me: " + count;
-};
+}
+
+var inc_button = document.getElementById("clickme");
+	count = 0;
+inc_button.addEventListener("click", inc);
 
 var res_button = document.getElementById("reset");
-res_button.onclick = function() {
+res_button.addEventListener("click", function() {
   if(window.confirm("Are you sure you want to reset the counter?")) {
 	  count = rewards.rewards[0].streak;
   	inc_button.innerHTML = "Click me: " + count;
   }
-};
+});
 
 var add_button = document.getElementById("add");
-add_button.onclick = function() {
-	var btn = document.createElement("BUTTON");
+add_button.addEventListener( 'click', function() {
+/*	var btn = document.createElement("BUTTON");
 	var btn_txt = document.createTextNode("mmmhhhmmm");
 	btn.appendChild(btn_txt);
 	var linebreak = document.createElement("br");
-	
+*/
+	var linebreak = document.createElement("br");
 	document.getElementById("reward_list").appendChild(linebreak);
-	document.getElementById("reward_list").appendChild(btn);
+	document.getElementById("reward_list").appendChild(create_entry());
 	
-};
+	
+});
+
+function create_entry() 
+{
+	var spn = document.createElement('span');
+	var btn = document.createElement("BUTTON");
+	var item_txt = prompt("What's the reward gon' be?");
+	var desc_txt = prompt("Whatcha gotta do?");
+	var targ = Number( prompt("How many days you gotta do it?") );
+	console.log(rewards)
+	rewards["rewards"].push( {"item":item_txt, "streak":0, "target":targ, "description":desc_txt} );
+	
+	var btn_txt = document.createTextNode(item_txt);
+	
+	var btn2 = document.createElement("BUTTON");
+	var btn_txt2 = document.createTextNode("lol");
+	btn.appendChild(btn_txt);
+	btn2.appendChild(btn_txt2);
+	spn.appendChild(btn);
+	spn.appendChild(btn2);
+	return spn	;
+}
