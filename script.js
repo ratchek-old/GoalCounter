f43818019269e53123d85c79dfea984009695daa
ratchek <git@ratchek.com>
2018-09-09 07:58:34 -0400

    Add structure to html generated by js

--- script.js
+++ script.js
@@ -5,23 +5,6 @@ var rewards_text = '{ "rewards" : ['+
 
 var rewards = JSON.parse(rewards_text);
 
-function inc() {
-  count += 1;
-  inc_button.innerHTML = "Click me: " + count;
-}
-
-var inc_button = document.getElementById("clickme");
-	count = 0;
-inc_button.addEventListener("click", inc);
-
-var res_button = document.getElementById("reset");
-res_button.addEventListener("click", function() {
-  if(window.confirm("Are you sure you want to reset the counter?")) {
-	  count = rewards.rewards[0].streak;
-  	inc_button.innerHTML = "Click me: " + count;
-  }
-});
-
 var add_button = document.getElementById("add");
 add_button.addEventListener( 'click', function() {
 /*	var btn = document.createElement("BUTTON");
@@ -51,14 +34,49 @@ function create_entry()
 function entry_html(index){
 	var entry = rewards["rewards"][index]
 	var spn = document.createElement('span');
-	var btn = document.createElement("BUTTON");
-	var btn_txt = document.createTextNode(entry["item"]);
+	
+	var hd = document.createElement('h3');
+	var txt = document.createTextNode(entry["item"]);
+	
+	var count_btn = document.createElement("BUTTON");
+	var btn_txt = document.createTextNode(entry["streak"]);
 	
 	var btn2 = document.createElement("BUTTON");
 	var btn_txt2 = document.createTextNode(entry["target"]);
-	btn.appendChild(btn_txt);
+	
+	var par = document.createElement('p');
+	var par_txt = document.createTextNode(entry["description"]);
+	
+	count_btn.appendChild(btn_txt);
 	btn2.appendChild(btn_txt2);
-	spn.appendChild(btn);
+	par.appendChild(par_txt);
+	hd.appendChild(txt);
+	hd.appendChild(count_btn);
+	hd.appendChild(btn2);
+	
+
+	spn.appendChild(hd)
+/*	spn.appendChild(count_btn);
 	spn.appendChild(btn2);
+*/	spn.appendChild(par)
 	return spn	;
 }
+
+
+function inc() {
+  count += 1;
+  inc_button.innerHTML = "Click me: " + count;
+}
+
+var inc_button = document.getElementById("clickme");
+	count = 0;
+inc_button.addEventListener("click", inc);
+
+var res_button = document.getElementById("reset");
+res_button.addEventListener("click", function() {
+  if(window.confirm("Are you sure you want to reset the counter?")) {
+	  count = rewards.rewards[0].streak;
+  	inc_button.innerHTML = "Click me: " + count;
+  }
+});
+
